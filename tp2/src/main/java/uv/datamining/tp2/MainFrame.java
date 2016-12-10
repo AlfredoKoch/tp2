491a87f2e81e3eb32661f297e4cdb3f0c2e9739d
R4 <R4@BLOODARENA>
2016-12-09 20:54:26 -0500

    Se implementa el modelo NaiveBayes. Falta el random forest, la evaluación con los datos de prueba y la integración final

--- tp2/src/main/java/uv/datamining/tp2/MainFrame.java
+++ tp2/src/main/java/uv/datamining/tp2/MainFrame.java
@@ -25,15 +25,26 @@ import org.apache.poi.xssf.usermodel.XSSFWorkbook;
  */
 public class MainFrame extends javax.swing.JFrame {
 
-    File file;
+    private File file;
+    private WekaModeler wekaModeler;
+    
+    private TextAreaOutputStream consola;
     
     /**
      * Creates new form MainFrame
      */
     public MainFrame() {
         initComponents();
+        consola = new TextAreaOutputStream(jTextArea1);
+        wekaModeler = new WekaModeler(this);
     }
-
+    
+    public void appendMessage(String message){
+        consola.write(message.getBytes());
+    }
+    
+    
+    
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
@@ -52,10 +63,18 @@ public class MainFrame extends javax.swing.JFrame {
         jLabel1 = new javax.swing.JLabel();
         jTextField1 = new javax.swing.JTextField();
         jButton5 = new javax.swing.JButton();
+        jLabel2 = new javax.swing.JLabel();
+        jTextField2 = new javax.swing.JTextField();
+        jButton6 = new javax.swing.JButton();
+        jLabel3 = new javax.swing.JLabel();
+        jTextField3 = new javax.swing.JTextField();
+        jButton7 = new javax.swing.JButton();
+        jLabel4 = new javax.swing.JLabel();
+        jTextField4 = new javax.swing.JTextField();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-        setPreferredSize(new java.awt.Dimension(600, 600));
 
+        jTextArea1.setEditable(false);
         jTextArea1.setColumns(20);
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
@@ -67,18 +86,30 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
 
-        jButton2.setText("Entrenar Modelo (WEKA)");
+        jButton2.setText("Entrenar Modelo (WEKA J48)");
         jButton2.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jButton2ActionPerformed(evt);
             }
         });
 
-        jButton3.setText("Probar");
+        jButton3.setText("Cargar Modelo (WEKA J48)");
+        jButton3.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton3ActionPerformed(evt);
+            }
+        });
 
-        jButton4.setText("Clasificar");
+        jButton4.setText("Entrenar Modelo (WEKA BAYES)");
+        jButton4.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton4ActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("Archivo:");
+
+        jTextField1.setEditable(false);
 
         jButton5.setText("Convertir CSV a ARFF");
         jButton5.addActionListener(new java.awt.event.ActionListener() {
@@ -87,6 +118,32 @@ public class MainFrame extends javax.swing.JFrame {
             }
         });
 
+        jLabel2.setText("CM");
+
+        jTextField2.setText("0.1");
+
+        jButton6.setText("Limpiar Consola");
+        jButton6.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton6ActionPerformed(evt);
+            }
+        });
+
+        jLabel3.setText("Modelo J48:");
+
+        jTextField3.setEditable(false);
+
+        jButton7.setText("Cargar Modelo (WEKA BAYES)");
+        jButton7.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton7ActionPerformed(evt);
+            }
+        });
+
+        jLabel4.setText("Modelo Bayes");
+
+        jTextField4.setEditable(false);
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -94,18 +151,31 @@ public class MainFrame extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 533, Short.MAX_VALUE)
+                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 536, Short.MAX_VALUE)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
+                        .addGap(0, 0, Short.MAX_VALUE)
+                        .addComponent(jButton6))
                     .addGroup(layout.createSequentialGroup()
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING, false)
+                            .addComponent(jButton7, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton5, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton2, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton3, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                             .addComponent(jButton4, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
                         .addGap(34, 34, 34)
-                        .addComponent(jLabel1)
-                        .addGap(18, 18, 18)
-                        .addComponent(jTextField1)))
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addComponent(jLabel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                            .addComponent(jLabel3, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                            .addComponent(jLabel4, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                            .addComponent(jTextField3)
+                            .addComponent(jTextField1)
+                            .addComponent(jTextField2, javax.swing.GroupLayout.DEFAULT_SIZE, 230, Short.MAX_VALUE)
+                            .addComponent(jTextField4))))
                 .addContainerGap())
         );
         layout.setVerticalGroup(
@@ -117,16 +187,29 @@ public class MainFrame extends javax.swing.JFrame {
                     .addComponent(jLabel1)
                     .addComponent(jTextField1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton5)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton5)
+                    .addComponent(jLabel2)
+                    .addComponent(jTextField2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton2)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton2)
+                    .addComponent(jLabel3)
+                    .addComponent(jTextField3, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jButton3)
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jButton3)
+                    .addComponent(jLabel4)
+                    .addComponent(jTextField4, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton4)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 77, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton7)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 63, Short.MAX_VALUE)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 199, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap())
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jButton6)
+                .addGap(6, 6, 6))
         );
 
         pack();
@@ -169,24 +252,92 @@ public class MainFrame extends javax.swing.JFrame {
 
     private void jButton5ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton5ActionPerformed
         try {
-            file = new File(WekaModeler.csvToArff(file));
+            file = new File(wekaModeler.csvToArff(file));
             jTextField1.setText(file.getName());
             JOptionPane.showMessageDialog(this, "Conversión Finalizada");
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(this, "Error al convertir archivo : "+ex.getMessage());
+            this.appendMessage(ex.getMessage());
             Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jButton5ActionPerformed
 
     private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
         try {
-            WekaModeler.generarArbol(file, 0.1F);
+            Float cm = Float.valueOf(jTextField2.getText());
+            wekaModeler.generarArbolJ48(file, cm);
             JOptionPane.showMessageDialog(null, "Árbol Generado");
+        }catch(NumberFormatException e){
+            JOptionPane.showMessageDialog(this, "El valor del CM no es un número válido");
         } catch (Exception ex) {
             JOptionPane.showMessageDialog(this, "Error al generar el arbol : "+ex.getMessage());
+            this.appendMessage(ex.getMessage());
             Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
         }
     }//GEN-LAST:event_jButton2ActionPerformed
+
+    private void jButton6ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton6ActionPerformed
+        consola.clear();
+    }//GEN-LAST:event_jButton6ActionPerformed
+
+    private void jButton3ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton3ActionPerformed
+        try {
+            JFileChooser chooser = new JFileChooser();
+            chooser.showOpenDialog(this);
+            File file = chooser.getSelectedFile();
+            if (file == null) {
+                JOptionPane.showMessageDialog(this, "Error al cargar el archivo");
+                return;
+            }
+            if(!file.getName().contains("modelJ48")){
+                JOptionPane.showMessageDialog(this, "Por favor seleccione un archivo válido (.modelJ48)");
+                return;
+            }
+            wekaModeler.readModelJ48(file.getAbsolutePath());
+            jTextField3.setText(file.getName());
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error :"+ex.getMessage());
+            this.appendMessage(ex.getMessage());
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+    }//GEN-LAST:event_jButton3ActionPerformed
+
+    private void jButton4ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton4ActionPerformed
+        if(file==null || file.getName().isEmpty() || !file.getName().contains(".arff")){
+            JOptionPane.showMessageDialog(this, "Por favor cargue un archivo .arff");
+            return;
+        }
+        try {
+            wekaModeler.generarBayes(file);
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error :"+ex.getMessage());
+            this.appendMessage(ex.getMessage());
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton4ActionPerformed
+
+    private void jButton7ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton7ActionPerformed
+        try {
+            JFileChooser chooser = new JFileChooser();
+            chooser.showOpenDialog(this);
+            File file = chooser.getSelectedFile();
+            if (file == null) {
+                JOptionPane.showMessageDialog(this, "Error al cargar el archivo");
+                return;
+            }
+            if(!file.getName().contains("modelJ48")){
+                JOptionPane.showMessageDialog(this, "Por favor seleccione un archivo válido (.modelBayes)");
+                return;
+            }
+            wekaModeler.readModelBayes(file.getAbsolutePath());
+            jTextField4.setText(file.getName());
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(this, "Error :"+ex.getMessage());
+            this.appendMessage(ex.getMessage());
+            Logger.getLogger(MainFrame.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }//GEN-LAST:event_jButton7ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -229,9 +380,17 @@ public class MainFrame extends javax.swing.JFrame {
     private javax.swing.JButton jButton3;
     private javax.swing.JButton jButton4;
     private javax.swing.JButton jButton5;
+    private javax.swing.JButton jButton6;
+    private javax.swing.JButton jButton7;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
     private javax.swing.JTextField jTextField1;
+    private javax.swing.JTextField jTextField2;
+    private javax.swing.JTextField jTextField3;
+    private javax.swing.JTextField jTextField4;
     // End of variables declaration//GEN-END:variables
 }
--- tp2/src/main/java/uv/datamining/tp2/WekaModeler.java
+++ tp2/src/main/java/uv/datamining/tp2/WekaModeler.java
@@ -12,6 +12,7 @@ import weka.core.converters.ArffSaver;
 import weka.core.converters.CSVLoader;
 import weka.classifiers.trees.J48;
 import weka.core.converters.ArffLoader;
+import weka.classifiers.bayes.NaiveBayes;
 
 /**
  *
@@ -19,7 +20,27 @@ import weka.core.converters.ArffLoader;
  */
 public class WekaModeler {
     
-    public static String csvToArff(File csv) throws Exception{
+    private MainFrame frame;
+    private J48 modeloJ48;
+    private NaiveBayes modeloBayes;
+    
+    
+    
+    
+    public WekaModeler(MainFrame frame){
+        this.frame = frame;
+    }
+    
+    //<>
+    /**
+     * Genera un archivo de formato ARFF a partir de un archivo .SCV
+     * Lo genera en el mismo directorio con nombre archivo_csv.arff
+     * 
+     * @param csv
+     * @return
+     * @throws Exception 
+     */
+    public String csvToArff(File csv) throws Exception{
         if(!csv.getName().endsWith(".csv")){
             throw new Exception("El archivo "+csv.getAbsolutePath()+" no es un archivo .csv");
         }
@@ -31,7 +52,6 @@ public class WekaModeler {
         ArffSaver saver = new ArffSaver();
         saver.setInstances(data);
         String arff = csv.getAbsolutePath().substring(0,csv.getAbsolutePath().lastIndexOf(".")) +".arff";
-        System.out.println(arff);
         saver.setFile(new File(arff));
 //        saver.setDestination(new File(arff));
         saver.writeBatch();
@@ -39,20 +59,66 @@ public class WekaModeler {
         return arff;
     }
     
-    public static void generarArbol(File file, float cm) throws Exception{
+    /**
+     * Genera un arbol de clasificación de tipo J48 de Weka.
+     * Serializa el arbol con nombre archivo_arff.model
+     * 
+     * @param file
+     * @param cm
+     * @throws Exception 
+     */
+    public void generarArbolJ48(File file, float cm) throws Exception{
         ArffLoader loader = new ArffLoader();
         loader.setFile(file);
         Instances data = loader.getDataSet();
         data.setClassIndex(data.numAttributes() - 1); //columna con el atributo clase
-        J48 tree = new J48();
-        tree.setConfidenceFactor(cm);
-        tree.buildClassifier(data);
+        modeloJ48 = new J48();
+        modeloJ48.setConfidenceFactor(cm);
+        modeloJ48.buildClassifier(data);
         Evaluation eval = new Evaluation(data);
-        eval.evaluateModel(tree, data);
-        System.out.println(eval.toSummaryString());
+        eval.evaluateModel(modeloJ48, data);
+        frame.appendMessage("--- MODELO J48 ---");
+        frame.appendMessage(eval.toSummaryString());
+        frame.appendMessage(eval.toMatrixString());
+        frame.appendMessage("--- FIN MODELO J48 ---");
         
-        weka.core.SerializationHelper.write(file.getAbsolutePath().substring(0,file.getAbsolutePath().lastIndexOf(".")) +".model",tree);
         
+        weka.core.SerializationHelper.write(file.getAbsolutePath().substring(0,file.getAbsolutePath().lastIndexOf(".")) +".modelJ48",modeloJ48);
+        
+    }
+    
+    /**
+     * Genera un arbol de clasificación de tipo J48 de Weka.
+     * Serializa el arbol con nombre archivo_arff.model
+     * 
+     * @param file
+     * @param cm
+     * @throws Exception 
+     */
+    public void generarBayes(File file) throws Exception{
+        ArffLoader loader = new ArffLoader();
+        loader.setFile(file);
+        Instances data = loader.getDataSet();
+        data.setClassIndex(data.numAttributes() - 1); //columna con el atributo clase
+        modeloBayes = new NaiveBayes();
+        modeloBayes.buildClassifier(data);
+        Evaluation eval = new Evaluation(data);
+        eval.evaluateModel(modeloBayes, data);
+        frame.appendMessage("--- MODELO J48 ---");
+        frame.appendMessage(eval.toSummaryString());
+        frame.appendMessage(eval.toMatrixString());
+        frame.appendMessage("--- FIN MODELO J48 ---");
+        
+        weka.core.SerializationHelper.write(file.getAbsolutePath().substring(0,file.getAbsolutePath().lastIndexOf(".")) +".modelBayes",modeloJ48);
+        
+    }
+
+    void readModelJ48(String absolutePath) throws Exception {
+        modeloJ48 = (J48) weka.core.SerializationHelper.read(absolutePath);
+    }
+    
+    void readModelBayes(String absolutePath) throws Exception {
+        modeloBayes = (NaiveBayes) weka.core.SerializationHelper.read(absolutePath);
     }
     
 }
